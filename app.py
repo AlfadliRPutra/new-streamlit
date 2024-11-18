cf1fbc02886823533f3675bba7d6b5b509e9b9a5
Alfadli Rahmat Putra <121837863+AlfadliRPutra@users.noreply.github.com>
2024-11-18 22:06:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,16 +50,25 @@ def invert_scale(scaler, X, value):
 
 # Melatih model LSTM
 def fit_lstm(train, batch_size, nb_epoch, neurons):
+    # Menyiapkan input dan output
     X, y = train[:, 0:-1], train[:, -1]
-    X = X.reshape(X.shape[0], 1, X.shape[1])
+    
+    # Pastikan input X memiliki tiga dimensi: [samples, timesteps, features]
+    X = X.reshape(X.shape[0], 1, X.shape[1])  # [samples, 1 timestep, features per timestep]
+
+    # Membuat model LSTM
     model = Sequential()
-    model.add(LSTM(neurons, input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
-    model.add(Dense(1))
+    model.add(LSTM(neurons, input_shape=(X.shape[1], X.shape[2]), stateful=True))  # [timesteps, features]
+    model.add(Dense(1))  # Output layer
     model.compile(loss='mean_squared_error', optimizer='adam')
+
+    # Melatih model LSTM
     for i in range(nb_epoch):
         model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
         model.reset_states()
+    
     return model
+
 
 # Memprediksi nilai
 def forecast_lstm(model, batch_size, X):
