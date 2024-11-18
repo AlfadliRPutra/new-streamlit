d708070c79ee6e48ae6f35770cf3c34924216794
Alfadli Rahmat Putra <121837863+AlfadliRPutra@users.noreply.github.com>
2024-11-19 00:20:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def fit_lstm(train, batch_size, nb_epoch, neurons):
 	X, y = train[:, 0:-1], train[:, -1]
 	X = X.reshape(X.shape[0], 1, X.shape[1])
 	model = Sequential()
-	model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
+	model.add(LSTM(neurons,stateful=True))
 	model.add(Dense(1))
 	model.compile(loss='mean_squared_error', optimizer='adam')
 	for i in range(nb_epoch):
